a5653e72d11472dfc93ece04873255f8496cb2e2
fulu90 <giantsspirit2016@gmail.com>
2021-03-23 08:31:49 +0900

    モデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
   validates :nickname, presence: true
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "Full-width characters"} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'Full-width characters' } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: {with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"} do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_kana
     validates :first_name_kana
   end
