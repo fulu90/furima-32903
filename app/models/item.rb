3ca3bc1ccc0152cd909d582aed03e6ca09ccbf50
fulu90 <giantsspirit2016@gmail.com>
2021-03-28 15:32:28 +0900

    モデル設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :shipping_date
+
+  validates :product_name,:description,:selling_price, presence: true
+
+  validates :category_id,:status_id,:postage_id,:region_id,
+            :shipping_date_id, numericality: { other_than: 1 }
 end
