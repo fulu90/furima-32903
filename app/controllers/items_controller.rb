50a7190ae9ee87f947ed3a3682abe61c4ab84486
fulu90 <giantsspirit2016@gmail.com>
2021-04-04 20:17:55 +0900

    商品編集実装_修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_action, only: [:new, :create, :edit, :update]
   before_action :redirect_root, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.all.order('created_at ASC')
@@ -20,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -43,8 +41,8 @@ class ItemsController < ApplicationController
                                  :selling_price).merge(user_id: current_user.id)
   end
 
-  def move_action
-    redirect_to new_user_session_path unless user_signed_in?
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def redirect_root
